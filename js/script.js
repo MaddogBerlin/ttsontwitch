9b3a796eab152ffe0d00396e56e4ee9dfb573c2d
MaddogBerlin <67278728+MaddogBerlin@users.noreply.github.com>
2026-05-18 00:29:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,3 +28,11 @@ if (userData) {
   document.getElementById("userName").textContent = name;
   document.getElementById("userInfo").classList.remove("hidden");
 }
+
+function updateClock() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  document.getElementById("systemClock").textContent = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
